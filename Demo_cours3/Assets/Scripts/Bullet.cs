39de8841a7287781f341862c13c2e482dcb10811
B533P-08\533 <pluc113@hotmail.com>
2018-08-27 16:56:40 -0400

    gun incomplet

---
+++ Demo_cours3/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        Vector3 ajustedMovement = transform.TransformDirection(Vector3.forward);
+        rigidbody.AddForce(ajustedMovement * 1000);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
